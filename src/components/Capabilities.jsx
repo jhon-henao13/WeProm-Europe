e5f0bcf166682c4425bf3d08a72a85705ea10c27
jhon-henao13 <jhonestiwarhenao@gmail.com>
2026-03-19 18:32:23 -0500

    Responsive in Capabilities

--- src/components/Capabilities.jsx
+++ src/components/Capabilities.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 export const Capabilities = () => {
   return (
-    <section className="relative bg-white py-32 px-8 md:px-16 overflow-hidden border-b border-slate-50">
+    <section className="relative bg-white py-22 px-8 md:px-16 overflow-hidden border-b border-slate-50">
       <div className="max-w-full mx-auto flex flex-col md:flex-row items-center">
         
         {/* LADO IZQUIERDO: 60% de ancho */}
         <div className="w-full md:w-[70%] z-10">
-          <h2 className="reveal text-blue-600 text-[60px] font-bold uppercase mb-12">
+          <h2 className="reveal text-blue-600 text-4xl md:text-[60px] font-bold uppercase mb-8 md:mb-12 leading-tight">
             Strategic Capabilities
           </h2>
           
           <div className="space-y-16">
             {/* Capacidad 1 */}
             <div className="reveal" style={{ transitionDelay: '0.2s' }}>
-              <h3 className="text-[0.7rem] md:text-[1.3rem] font-semibold mb-6 text-weprom-dark tracking-[0.44rem]">
+
+              <h3 className="text-lg md:text-[1.3rem] font-semibold mb-4 md:mb-6 text-weprom-dark tracking-[0.22em] md:tracking-[0.44rem] leading-relaxed">
                 Market Research & Business Intelligence
               </h3>
               <p className="text-black font-montserrat leading-snug text-lg font-normal max-w-8xl tracking-wide">
@@ -27,7 +28,7 @@ export const Capabilities = () => {
 
             {/* Capacidad 2 */}
             <div className="reveal" style={{ transitionDelay: '0.4s' }}>
-              <h3 className="text-[0.7rem] md:text-[1.2rem] font-semibold mb-6 text-weprom-dark tracking-[0.44rem]">
+              <h3 className="text-lg md:text-[1.3rem] font-semibold mb-4 md:mb-6 text-weprom-dark tracking-[0.22em] md:tracking-[0.44rem] leading-relaxed">
                 International Expansion & Market Entry
               </h3>
               <p className="text-black font-montserrat leading-snug text-lg font-normal max-w-2xl max-w-5xl tracking-wide">
